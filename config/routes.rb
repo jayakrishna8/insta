1a2c6a33fb690daa7d2c8f913973a17537745134
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-08-08 22:27:24 +0530

    Integrated ui for user updating profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
   root to: 'home#index'
 
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy]
+  get 'search' => 'search#index'
 end
